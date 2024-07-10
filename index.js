399e52e5363d961057d4aaa40d8376fd4e3c754d
Anna Bothe <annabothe0@gmail.com>
2024-07-10 15:06:52 +0200

    refactored code with jQuery and made it playable

--- index.js
+++ index.js
@@ -1,72 +1,139 @@
 var sounds = [
-  new Audio('sounds/green.mp3'),
   new Audio('sounds/red.mp3'),
-  new Audio('sounds/yellow.mp3'),
   new Audio('sounds/blue.mp3'),
+  new Audio('sounds/green.mp3'),
+  new Audio('sounds/yellow.mp3'),
   new Audio('sounds/wrong.mp3')
 ];
 
 var buttons = document.querySelectorAll('.btn');
+var buttonColors = ["red", "blue", "green", "yellow"];
 var gameStart = false;
-var gameOver = false;
+// var gameOver = false;
 var gamePattern = [];
 var userClickedPattern = [];
+// var eventCounter = 0;
+var level = 0;
 
-document.addEventListener('keydown', function() {
-  startGame();
-  computerTurn();
-  userTurn();
+$(document).keypress(function() {
+  if (!gameStart) {
+    nextSequence();
+    gameStart = true;
+  }
 });
 
-function startGame() {
-  if (gameStart) return;
-  gameStart = true;
-  // console.log('Game started');
-};
-
-function computerTurn() {
-  document.getElementById("level-title").innerText = "Level " + (1 + gamePattern.length);
+function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
-  var randomButton = buttons[randomNumber];
-  gamePattern.push(randomButton.id);
-  randomButton.classList.add('pressed');
-  sounds[randomNumber].play();
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  var audio = new Audio('sounds/' + randomChosenColor + '.mp3');
+  audio.play();
+  level++;
+  $("#level-title").text("Level " + level);
+};
+
+$(".btn").click(function() {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
+  // console.log(userClickedPattern);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+});
+
+function playSound(name) {
+  $("#" + name).fadeIn(100).fadeOut(100).fadeIn(100);
+  var audio = new Audio('sounds/' + name + '.mp3');
+  audio.play();
+};
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
   setTimeout(function() {
-    randomButton.classList.remove('pressed');
-  }, 200);
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 };
 
-function userTurn() {
-  buttons.forEach(function(button) {
-    button.addEventListener("click", function() {
-      var userClickedButton = button.id;
-      userClickedPattern.push(userClickedButton);
-      if (userClickedPattern === gamePattern) {
-        userClickedButton.classList.add('pressed');
-        sounds[userClickedButton].play();
-        setTimeout(function() {
-          userClickedButton.classList.remove('pressed');
-        }, 200);
-        gameLoop();
-      } else {
-        gameOver = true;
-        sounds[5].play();
-        document.getElementById("level-title").innerText = "Game Over";
-        document.body.addClassList('game-over');
-      }
-    });
-  });
+function checkAnswer(currentLevel) {
+  if(userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
+    if(userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+        userClickedPattern = [];
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+    }, 200);
+    startOver();
+  }
 };
 
-// after game starts, the user waits for the computer to make a move
-// the computer makes a move by randomly selecting a button
-// this button then makes the right sound and flashes according to the color
-// the button the computer click get added to the gamePattern array
-// the user then has to click the same button that the computer clicked
-// the user's button click gets added to the userClickedPattern array
-// the user's button click gets compared to the computer's button click
-// if the user's button click is the same as the computer's button click,
-// the button makes the right sound and flashes according to the color
-// if the user's button click is different from the computer's button click,
-// the button makes the wrong sound and flashes red and the game is over
-// this process repeats until the user's button click is different from the computer's button click
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+  gameStart = false;
+};
+
+
+
+// document.addEventListener('keydown', function(event) {
+//   // startGame();
+//   if(eventCounter % 2 === 0) {
+//     computerTurn();
+//   } else if(eventCounter % 2 !== 0) {
+//     userTurn();
+//   };
+// });
+
+// // function startGame() {
+// //   if (gameStart) return;
+// //   gameStart = true;
+// //   // console.log('Game started');
+// // };
+
+// function computerTurn() {
+//   document.getElementById("level-title").innerText = "Level " + (1 + gamePattern.length);
+//   document.getElementById("turn-title").innerText = "Computer's Turn";
+//   var randomNumber = Math.floor(Math.random() * 4);
+//   var randomButton = buttons[randomNumber];
+//   gamePattern.push(randomButton.id);
+//   randomButton.classList.add('pressed');
+//   sounds[randomNumber].play();
+//   setTimeout(function() {
+//     randomButton.classList.remove('pressed');
+//   }, 200);
+//   eventCounter++;
+// };
+
+// function userTurn() {
+//   document.getElementById("turn-title").innerText = "Your Turn";
+//   buttons.forEach(function(button) {
+//     button.addEventListener("click", function() {
+//       var userClickedButton = button.id;
+//       userClickedPattern.push(userClickedButton);
+//       if (userClickedPattern[i] === gamePattern[i]) {
+//         userClickedButton.classList.add('pressed');
+//         sounds[userClickedButton].play();
+//         setTimeout(function() {
+//           userClickedButton.classList.remove('pressed');
+//         }, 200);
+//       } else {
+//         gameOver = true;
+//         sounds[5].play();
+//         document.getElementById("level-title").innerText = "Game Over";
+//         document.body.addClassList('game-over');
+//         document.getElementById("turn-title").classList.remove('turn-title');
+//       }
+//     });
+//   });
+//   eventCounter++;
+// };
